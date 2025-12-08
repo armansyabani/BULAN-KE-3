670e7a9f51110e1521f3dd730197a50a0ff0f5e0
armansyabani <armansyabni@gmail.com>
2025-12-08 23:23:03 +0700

    portopolio

---
+++ PEKAN-2/DAY-1/vite-project/src/components/ProfileBox.jsx
@@ -0,0 +1,10 @@
+function ProfileBox() {
+  return (
+    <div className="profile-box">
+      <h2>Ini adalah ProfileBox</h2>
+      <p>Komponen dengan inline style diganti ke CSS biar rapi.</p>
+    </div>
+  );
+}
+
+export default ProfileBox;
