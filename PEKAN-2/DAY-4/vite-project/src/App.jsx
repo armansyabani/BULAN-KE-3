7397a6dbd3824fb329dc999169fc1f1e5afa7cbe
armansyabani <armansyabni@gmail.com>
2025-12-13 08:10:52 +0700

    feat: initialize Vite project with React and essential components
    - Added package.json with dependencies for React, Axios, and ESLint.
    - Created main application structure including App.jsx and CSS styles.
    - Implemented multiple data fetching components: FetchDataComponent, AxiosDataComponent, AsyncAwaitInEffect, and AbortFetchComponent.
    - Added UserListManipulated component to demonstrate data manipulation and filtering.
    - Included SVG assets for React and Vite logos.
    - Configured Vite with a basic vite.config.js file.
    - Established global styles in index.css for consistent theming.

---
+++ PEKAN-2/DAY-4/vite-project/src/App.jsx
@@ -0,0 +1,22 @@
+import AsyncAwaitInEffect from "./components/AsyncAwaitInEffect"
+import AxiosDataComponent from "./components/AxiosDataComponent"
+import FetchDataComponent from "./components/FetchDataComponent"
+import AbortFetchComponent from "./components/AbortFetchComponent"
+import UserListManipulated from "./components/UserListManipulated"
+
+
+function App() {
+
+
+  return (
+    <>
+    {/* <FetchDataComponent /> */}
+    {/* <AxiosDataComponent /> */}
+    {/* <AsyncAwaitInEffect />
+    <AbortFetchComponent /> */}
+    <UserListManipulated />
+    </>
+  )
+}
+
+export default App
