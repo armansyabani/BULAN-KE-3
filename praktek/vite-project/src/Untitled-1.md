6792d5b0e1479254e8216965de9518b0bb8cb73f
armansyabani <armansyabni@gmail.com>
2025-12-20 08:23:49 +0700

    feat: Initialize Vite project with React and React Router
    - Created package.json with dependencies for React, React Router, and development tools.
    - Added Vite configuration for building and serving the application.
    - Implemented basic routing structure with Home, About, Contact, ProductDetail, and Dashboard pages.
    - Added nested routes for Dashboard with Profile and Settings subpages.
    - Created NotFound page for handling 404 errors.
    - Styled application with CSS for navigation and active links.
    - Included SVG assets for branding.

---
+++ praktek/vite-project/src/Untitled-1.md
@@ -0,0 +1,1 @@
+https://github.com/armansyabani/BULAN-KE-3/tree/main/PEKAN-2/EVALUASI-2/react-shopcd
---
+++ praktek/vite-project/src/pages/DashboardSettings.jsx
@@ -0,0 +1,6 @@
+// src/pages/DashboardSettings.jsx
+import React from 'react';
+function DashboardSettings() {
+  return <h3>Ini adalah Pengaturan Akun</h3>;
+}
+export default DashboardSettings;
